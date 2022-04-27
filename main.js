7926c12064169a852b06ca503f9a8278d5d2a4c2
LuisCW <92056981+LuisCW@users.noreply.github.com>
2022-04-26 19:49:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+//ligthbox
+const galeria = document.querySelectorAll('.imagenes');
+const lightbox = document.querySelector('.lightbox');
+const expandir = document.querySelector('.expandir');
+const cerrar = document.querySelector('.cerrar');
+const texto = document.querySelector('.light-text');
+
+galeria.forEach(image =>{
+    image.addEventListener('click', ()=>{
+        addImage(image.getAttribute('src'),image.getAttribute('alt'))
+        console.log(image.getAttribute('alt'))
+       
+    })
+})
+const addImage = (srcImage, altImage)=>{
+    lightbox.classList.add('activo');
+    expandir.src = srcImage;
+    texto.innerHTML = altImage
+}
+cerrar.addEventListener('click', ()=>{
+    lightbox.classList.remove('activo');  
+})
+lightbox.addEventListener('click', ()=>{
+    lightbox.classList.remove('activo');  
+})
+
+//menu lateral
+const menuResponsive = document.querySelector('.ind-responsive');
+const botonMenu = document.querySelector('.ind-icon-black');
+const botonMenuOcultar = document.querySelector('.ind-icon');
+const ocultarGaleria = document.querySelector('.Galeria');
+const footer = document.querySelector('.footer');
+
+botonMenu.addEventListener('click', ()=>{
+    menuResponsive.classList.add('activo');
+    botonMenu.classList.add('desactivo');
+    ocultarGaleria.classList.add('desactivado');
+    footer.classList.add('active');  
+})
+botonMenuOcultar.addEventListener('click', ()=>{
+    menuResponsive.classList.remove('activo');
+    botonMenu.classList.remove('desactivo');
+    ocultarGaleria.classList.remove('desactivado');
+    footer.classList.remove('active'); 
+})
+
+const mediaqueryList = window.matchMedia("(min-width: 1150px)");
+
+
+mediaqueryList.addEventListener("change", e =>{
+    menuResponsive.classList.remove('activo');
+    botonMenu.classList.remove('desactivo');
+    ocultarGaleria.classList.remove('desactivado');
+    footer.classList.remove('active'); 
+})
+
+//menu header
+
+const cabeceraResponsive = document.querySelector('.icon-responsive')
+const botonMenuCabecera = document.querySelector('.ind-icon-blanco')
+const botonMenuCabeceraOcultar = document.querySelector('.ind-icon-negro')
+
+botonMenuCabecera.addEventListener('click', ()=>{
+    cabeceraResponsive.classList.add('activo');
+    botonMenuCabecera.classList.add('desactivado');
+})
+botonMenuCabeceraOcultar.addEventListener('click', ()=>{
+    cabeceraResponsive.classList.remove('activo');
+    botonMenuCabecera.classList.remove('desactivado');
+})
