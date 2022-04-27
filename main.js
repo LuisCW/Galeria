8244bf9a591011c7fc3a29ddc5d8d8d780603c30
LuisCW <92056981+LuisCW@users.noreply.github.com>
2022-04-26 19:48:46 -0500

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const imgGaleria = document.querySelectorAll('.imagenes');
-    M.Materialbox.init(imgGaleria, {
-        inDuraton: 500,
-        outDuration:500,
-    });
-});
